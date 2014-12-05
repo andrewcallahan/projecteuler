94366f539c6dd725ef079d562c88c4433fc04f1d
Andrew Callahan <andrew.callahan@gmail.com>
2014-12-05 17:48:44 -0500

    clear up comment on 15

--- 15.rb
+++ 15.rb
@@ -1,7 +1,7 @@
 # Make a grid that is one bigger in each direction, since you start at (0,0) and need to go to (20,20)
 a = Array.new(21) { Array.new(21) { 1 } }
 
-#This starts with the second to last item in an array, and then goes to the first item (basically, reverse order of array skipping the last item)
+#This starts with the second to last cell in the second to last row, and then goes all the way to the first item (basically, reverse order of array skipping the last item)
 order = (-a.size..-2).to_a.reverse
 
 #Iterate through the array starting with the second to last item in the second to last array
